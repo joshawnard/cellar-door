55de353d9e74f1059806a6e8d7598b04e2e43669
Joshua Ard <ard.josh@gmail.com>
2019-02-28 19:19:10 -0500

    Add class methods to handle phone input and start algorithm

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import fs from 'fs';
+
 import GenerateWordsFromPhoneNumber from './GenerateWordsFromPhoneNumber';
 
 import './App.css';
+
+debugger;
+
+// TODO: import dictionary file as array
+// const dictionaryWords = fs.readFile('./sample_dictionary.txt').toString().split("\n");
+const dictionaryWords = [];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <GenerateWordsFromPhoneNumber />
+        <GenerateWordsFromPhoneNumber dictionaryWords={dictionaryWords} />
       </div>
     );
   }
--- src/GenerateWordsFromPhoneNumber.jsx
+++ src/GenerateWordsFromPhoneNumber.jsx
@@ -1,4 +1,18 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+const NUMBER_TO_LETTER_MAP = {
+  1: [],
+  2: ['a', 'b', 'c'],
+  3: ['d', 'e', 'f'],
+  4: ['g', 'h', 'i'],
+  5: ['j', 'k', 'l'],
+  6: ['m', 'n', 'o'],
+  7: ['p', 'q', 'r', 's'],
+  8: ['t', 'u', 'v'],
+  9: ['w', 'x', 'y', 'z'],
+  0: [],
+};
 
 class GenerateWordsFromPhoneNumbers extends Component {
   constructor(props) {
@@ -10,16 +24,41 @@ class GenerateWordsFromPhoneNumbers extends Component {
   }
 
   handleChange = (event) => {
-    this.setState({ phoneNumber: event.target.value }, () => {
+    const phoneNumber = event.target.value;
+
+    this.setState({ phoneNumber }, () => {
       this.splitPhoneNumber();
     });
   }
 
   splitPhoneNumber = () => {
     const { phoneNumber } = this.state;
-    const newPhoneNumber = phoneNumber.toString().split('');
+    const splitPhoneNumber = phoneNumber.toString().split('');
 
-    
+    // Start iterating through each individual digit
+    splitPhoneNumber.forEach((num) => {
+      // Iterate through each letter mapped to this digit
+      NUMBER_TO_LETTER_MAP[num].forEach((letter) => {
+        this.checkForLetterMatches(letter);
+      });
+    });
+  }
+
+  checkForLetterMatches = (letter) => {
+    const dictionaryWords = this.props;
+
+    dictionaryWords.forEach((word) => {
+      // const length = word.length;
+      const letters = word.split('');
+
+      letters.forEach((l) => {
+        if (l === letter) {
+          // continue iterating through available mapped
+          // letters and if the indices match, keep going
+          // otherwise return;
+        }
+      });
+    });
   }
 
   render() {
@@ -41,6 +80,12 @@ class GenerateWordsFromPhoneNumbers extends Component {
       </div>
     );
   }
+}
+
+GenerateWordsFromPhoneNumbers.propTypes = {
+  dictionaryWords: PropTypes.arrayOf(
+    PropTypes.string,
+  ),
 }
 
 export default GenerateWordsFromPhoneNumbers;
